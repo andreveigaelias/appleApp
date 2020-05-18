47b33fe0877369cdb7211ba5d747ff9d2a1dc032
Andre Elias <andreveigaelias@gmail.com>
2020-05-18 14:59:40 -0300

    teste apple login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  showAppleSignIn = false;
+  user = null;
+  constructor(private alertController: AlertController) {}
+  async ngOnInit() {
+    const { Device } = Plugins;
+    // Only show the Apple sign in button on iOS
 
-  constructor() {}
+    let device = await Device.getInfo();
+    this.showAppleSignIn = device.platform === 'ios';
+  }
 
+  openAppleSignIn() {
+    const { SignInWithApple } = Plugins;
+    SignInWithApple.Authorize()
+        .then(async (res) => {
+          if (res.response && res.response.identityToken) {
+            this.user = res.response;
+          } else {
+            this.presentAlert();
+          }
+        })
+        .catch((response) => {
+          this.presentAlert();
+        });
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Login Failed',
+      message: 'Please try again later',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
